45a2e0a0ef17efdefd16999acd82d02979680777
Winfxk <Winfxk@qq.com>
2021-05-10 23:22:08 +0800

    Update 2021-05-10 23:22

---
+++ Automatic/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Automatic
+{
+    partial class Automatic
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Automatic));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Automatic
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Automatic.Properties.Resources.壁纸;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Automatic";
+            this.Opacity = 0.8D;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Automatic/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Automatic
+{
+    public partial class Automatic : Form
+    {
+        public Automatic()
+        {
+            InitializeComponent();
+        }
+        private void startCommand(String s)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(s);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            if (s == null || s.Length <= 0)
+            {
+                MessageBox.Show("请输入想要设置的倒计时时长！");
+                return;
+            }
+            if (!VldInt(s))
+            {
+                MessageBox.Show("倒计时仅支持纯数字！");
+                return;
+            }
+            double i = 0;
+            double.TryParse(s, out i);
+            if (i <= 0)
+            {
+                MessageBox.Show("倒计时长必须大于0！");
+                return;
+            }
+            startCommand("shutdown -s -t "+ (i*3600));
+            DialogResult dr1 = MessageBox.Show("已设置在"+s+"时("+(i*3600)+"秒)后关机！\n需要为您锁定计算机吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            if (dr1 == DialogResult.Yes)
+            {
+                startCommand("rundll32.exe user32.dll,LockWorkStation");
+            }
+            Process.GetCurrentProcess().Kill();
+        }
+        private bool VldInt(string num)
+        {
+            int ResultNum;
+            return int.TryParse(num, out ResultNum);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            startCommand("shutdown -a");
+            DialogResult dr1 = MessageBox.Show("已取消自动关机！\n退出程序吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            if (dr1 == DialogResult.Yes)
+            {
+                Process.GetCurrentProcess().Kill();
+                return;
+            }
+
+        }
+    }
+}
